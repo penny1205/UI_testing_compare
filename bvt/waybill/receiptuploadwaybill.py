54da6b418ab350f49543b99f4a06e65b063a2d28
liuxinyu <liuxinyu@keking.cn>
2017-09-21 14:55:51 +0800

    回单上传照片地址格式修改

--- bvt/waybill/receiptuploadwaybill.py
+++ bvt/waybill/receiptuploadwaybill.py
@@ -4,6 +4,7 @@
 import time
 import random
 import unittest
+import os
 from util.log.log import Log
 from util.file.fileutil import FileUtil
 from interface.waybill.waybill_detail_get import WayBillDetailGet
@@ -12,8 +13,9 @@ from interface.waybill.waybill_arrival_confirm import WayBillArrivalConfirm
 from interface.waybill.waybill_receipt_upload import WayBillReceiptUpload
 from bvt.common.createwaybill import CreateWayBill
 
+
 class TestReceiptUploadWayBill(unittest.TestCase):
-    '''回单上传'''
+    """回单上传"""
     def setUp(self):
         self.logger = Log()
         self.logger.info('####################### TestReceiptUploadWayBill START #######################')
@@ -27,20 +29,19 @@ class TestReceiptUploadWayBill(unittest.TestCase):
             arriveCity,'1000','10','0.01','0.02','0.03','0.04','1','测试','TMS','快递','10', '10', '10', '10', '10')
 
         global receipt_0
-        receipt_0 = FileUtil.getProjectObsPath() + '/image/logo.png'
-
+        receipt_0 = FileUtil.getProjectObsPath() + os.path.sep + 'image' + os.path.sep + 'logo.png'
 
     def tearDown(self):
         self.logger.info('######################## TestReceiptUploadWayBill END ########################')
         pass
 
     def test_receipt_upload_waybill_success(self):
-        '''回单上传'''
+        """回单上传"""
         WayBillDepartureConfirm().waybill_departure_confirm(wayBillId)
         waybill_transport_detail = WayBillDetailGet().waybill_detail_get(wayBillId).json()['content']
         WayBillArrivalConfirm().waybill_arrival_confirm(wayBillId,waybill_transport_detail['transportCash']['destAmt'],
                                                         waybill_transport_detail['transportCash']['destAmtMemo'])
-        response = WayBillReceiptUpload().waybill_receipt_upload(wayBillId,'Y','Y','N','回单上传测试','C',receipt_0)
+        response = WayBillReceiptUpload().waybill_receipt_upload(wayBillId, 'Y', 'Y', 'N', '回单上传测试', 'C', receipt_0)
         self.logger.info('回单上传返回状态码：{0}'.format(response))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['code'], 0)
