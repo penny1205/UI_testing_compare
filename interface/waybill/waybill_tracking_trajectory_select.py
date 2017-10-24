0e1df7a5c467bc749f576c89ae63f206e81fcb78
panyuanyuan <panyuanyuan@keking.cn>
2017-10-24 15:41:50 +0800

    更新

---
+++ interface/waybill/waybill_tracking_trajectory_select.py
@@ -0,0 +1,31 @@
+from util.config.yaml.readyaml import ReadYaml
+from util.http.httpclient import HttpClient
+from util.file.fileutil import FileUtil
+
+
+class WayBillTrackingTrajectorySelect(object):
+    '''
+    查询手动运单跟踪轨迹
+    /api/tms/wayBill/listManualTrajectory
+    '''
+    __slots__ = ('__wayBillTrackingTrajectorySelectApiUrl', '__head_dict')
+
+    def __init__(self):
+        config = ReadYaml(FileUtil.getProjectObsPath() + '/config/config.yaml').getValue()
+        self.__wayBillTrackingTrajectorySelectApiUrl = 'https://{0}:{1}{2}/api/tms/wayBill/listManualTrajectory'.\
+            format(config['tms_api_host'], config['tms_api_port'], config['tms_api_path'])
+        self.__head_dict = {
+            'token': config['tms_api_token'],
+            'YD_OAUTH': config['tms_api_YD_OAUTH']
+        }
+
+    def waybill_tracking_trajectory_select(self, wayBillId=''):
+        '''查询手动运单跟踪轨迹'''
+        try:
+            payload = {
+                'wayBillId': wayBillId,
+            }
+            response = HttpClient().get(self.__wayBillTrackingTrajectorySelectApiUrl,self.__head_dict,payload)
+            return response
+        except Exception:
+            return None
