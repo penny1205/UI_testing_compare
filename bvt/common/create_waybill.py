b42c384702fb80d7cf6f1c1295bcd2465ce85362
panyuanyuan <panyuanyuan@keking.cn>
2017-11-17 14:51:14 +0800

    更新

--- bvt/common/create_waybill.py
+++ bvt/common/create_waybill.py
@@ -270,7 +270,8 @@ class CreateWayBill(object):
             self.logger.info('获取已认证司机信息：{0}'.format(driver))
             driver = driver['content'][0]
         except Exception as error:
-            self.logger.info('获取认证司机信息失败,错误信息：'+error)
+            print(error)
+            self.logger.info('获取认证司机信息失败,错误信息：{0}'.format(error))
             return None
         # 查询认证司机是否有待发车状态的运单并处理
         waybill = WayBillSelect().waybill_select(billStatus='W',normalCondition=driver['mobile'],
--- bvt/test_finance/test_loan_update.py
+++ bvt/test_finance/test_loan_update.py
@@ -27,10 +27,10 @@ class TestLoanUpdate(unittest.TestCase):
         self.demandLoanDate_update = time.strftime('%Y-%m-%d')
         response = LoanUpdate().loan_update(self.Id,self.loanAmount,self.recipientName,self.recipientAccount,
                                             self.depositBank,self.remarks,self.demandLoanDate_update)
-        self.logger.info('删除大额贷款记录返回状态码：{0}'.format(response))
+        self.logger.info('修改大额贷款记录返回状态码：{0}'.format(response))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['code'], 0)
-        self.logger.info('删除大额贷款记录返回结果是：{0}'.format(response.json()))
+        self.logger.info('修改大额贷款记录记录返回结果是：{0}'.format(response.json()))
 
 if __name__ == '__main__':
     unittest.main()
