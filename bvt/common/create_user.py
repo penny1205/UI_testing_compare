2f567aacc0cd4f099460f54d195d147398e2a4b4
panyuanyuan <panyuanyuan@keking.cn>
2017-12-04 18:08:05 +0800

    需求变更：已删除子账号不能再新增

--- bvt/common/create_user.py
+++ bvt/common/create_user.py
@@ -3,6 +3,9 @@
 
 import random
 from util.log.log import Log
+from util.db.dbutil import DBUtil
+from util.file.fileutil import FileUtil
+from util.config.yaml.readyaml import ReadYaml
 from interface.permission.user_create import UserCreate
 from interface.permission.user_select import UserSelect
 from interface.permission.user_delete import UserDelete
@@ -15,6 +18,7 @@ class CreateUser(object):
 
     def __init__(self):
         self.logger = Log()
+        self.config = ReadYaml(FileUtil.getProjectObsPath() + '/config/config.yaml').getValue()
 
     def create_user(self,roleName,menuJson,name,userName,mobile,isLoginApp,carType):
         '''新增账号'''
@@ -35,20 +39,44 @@ class CreateUser(object):
             response = UserCreate().user_create(roleId,name,userName,mobile,isLoginApp,projectId,carType)
             if response.json()['code'] == 0:
                 return response.json()['content'],roleId
+            # 需求变动（已删除的子账号不能再新建）
+            # elif response.json()['code'] == 9110028:
+            #     user_list = UserSelect().user_select().json()['content']['dataList']
+            #     for user in user_list:
+            #         if user['userName'] == userName:
+            #             UserDelete().user_delete(user['loginId'])
+            #     userId = UserCreate().user_create(roleId,name,userName,mobile,isLoginApp,projectId,carType).json()['content']
+            #     return userId,roleId
+            # elif response.json()['code'] == 9110029:
+            #     user_list = UserSelect().user_select().json()['content']['dataList']
+            #     for user in user_list:
+            #         if user['mobile'] == mobile:
+            #             UserDelete().user_delete(user['loginId'])
+            #     userId = UserCreate().user_create(roleId,name,userName,mobile,isLoginApp,projectId,carType).json()['content']
+            #     return userId,roleId
             elif response.json()['code'] == 9110028:
-                user_list = UserSelect().user_select().json()['content']['dataList']
-                for user in user_list:
-                    if user['userName'] == userName:
-                        UserDelete().user_delete(user['loginId'])
-                userId = UserCreate().user_create(roleId,name,userName,mobile,isLoginApp,projectId,carType).json()['content']
-                return userId,roleId
+                sql = 'DELETE FROM YD_APP_APPROVAL_USER WHERE userName = \'{0}\' and partnerNo = \'{1}\''.format(
+                    userName,self.config['partnerNo'])
+                self.DBUtil = DBUtil(host=self.config['db_host'], port=self.config['db_port'],
+                                     user=self.config['db_user'], passwd=self.config['db_passwd'],
+                                     dbname=self.config['db_dbname'], charset=self.config['db_charset'])
+                print(sql)
+                self.DBUtil.execute_sql(sql)
+                userId = UserCreate().user_create(roleId, name, userName, mobile, isLoginApp, projectId, carType
+                                                  ).json()[ 'content']
+                return userId, roleId
             elif response.json()['code'] == 9110029:
-                user_list = UserSelect().user_select().json()['content']['dataList']
-                for user in user_list:
-                    if user['mobile'] == mobile:
-                        UserDelete().user_delete(user['loginId'])
-                userId = UserCreate().user_create(roleId,name,userName,mobile,isLoginApp,projectId,carType).json()['content']
-                return userId,roleId
+                sql = 'DELETE FROM YD_APP_APPROVAL_USER WHERE mobile = \'{0}\' and partnerNo = \'{1}\''.format(mobile,
+                                                                                             self.config['partnerNo'])
+                self.DBUtil = DBUtil(host=self.config['db_host'], port=self.config['db_port'],
+                                     user=self.config['db_user'], passwd=self.config['db_passwd'],
+                                     dbname=self.config['db_dbname'], charset=self.config['db_charset'])
+                print(sql)
+                self.DBUtil.execute_sql(sql)
+                print(sql)
+                userId = UserCreate().user_create(roleId, name, userName, mobile, isLoginApp, projectId, carType
+                                                  ).json()['content']
+                return userId, roleId
             else:
                 self.logger.info('新增账号返回错误:{0}'.format(response.json()))
                 return None,None
--- interface/permission/role_company_get.py
+++ interface/permission/role_company_get.py
@@ -10,12 +10,13 @@ class RoleCompanyGet(object):
     获取公司所有角色
     /api/tms/system/listTmsRoleByPartnerNo
     '''
-    __slots__ = ('__roleDeleteApiUrl', '__head_dict')
+    __slots__ = ('__roleDeleteApiUrl','partnerNo', '__head_dict')
 
     def __init__(self):
         config = ReadYaml(FileUtil.getProjectObsPath() + '/config/config.yaml').getValue()
         self.__roleDeleteApiUrl = 'https://{0}:{1}{2}/api/tms/system/listTmsRoleByPartnerNo'.format(
             config['tms_api_host'],config['tms_api_port'],config['tms_api_path'])
+        self.partnerNo = config['partnerNo']
         self.__head_dict = {
             'token': config['tms_api_token'],
             'YD_OAUTH': config['tms_api_YD_OAUTH'],
@@ -24,7 +25,10 @@ class RoleCompanyGet(object):
     def role_company_get(self):
          '''获取公司所有角色'''
          try:
-             response = HttpClient().get(self.__roleDeleteApiUrl,self.__head_dict)
+             payload = {
+                 'partnerNo': self.partnerNo,
+             }
+             response = HttpClient().get(self.__roleDeleteApiUrl,self.__head_dict,payload)
              return response
          except Exception:
              return None
