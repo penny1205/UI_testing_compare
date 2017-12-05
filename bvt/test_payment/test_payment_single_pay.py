dbf52771404ac9bde90b3ecb29d04cb42c0e1251
panyuanyuan <panyuanyuan@keking.cn>
2017-12-05 09:53:32 +0800

    注销钱包支付测试用例

--- bvt/test_payment/test_payment_single_pay.py
+++ bvt/test_payment/test_payment_single_pay.py
@@ -40,18 +40,18 @@ class SinglePay(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['code'], 0)
 
-    def test_singlePay_WALLET_TO_DRIVER(self):
-        """ 运费支付方式：钱包支付 """
-        waybillID = CreateWayBill().create_waybill_register().json()['content']
-        self.logger.info('新建运单，生成的运单号为{0}'.format(waybillID))
-        confirmMsg = WayBillDriverConfirm().waybill_driver_confirm(waybillID, totalAmt='0.05', preAmt='0.01',
-                                                                   oilAmt='0.01', destAmt='0.01', lastAmt='0.01')
-        self.logger.info('运单{0}，进行司机确认发车操作，返回信息：{1}'.format(waybillID, confirmMsg.json()))
-        response = PaymentSinglePay().single_pay(waybillID, paymentMethod=2, amountType=1, amount='0.01',
-                                                 password='123321')
-        self.logger.info('钱包付司机支付结果：{}'.format(response.json()))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['code'], 0)
+    # def test_singlePay_WALLET_TO_DRIVER(self):
+    #     """ 运费支付方式：钱包支付 """
+    #     waybillID = CreateWayBill().create_waybill_register().json()['content']
+    #     self.logger.info('新建运单，生成的运单号为{0}'.format(waybillID))
+    #     confirmMsg = WayBillDriverConfirm().waybill_driver_confirm(waybillID, totalAmt='0.05', preAmt='0.01',
+    #                                                                oilAmt='0.01', destAmt='0.01', lastAmt='0.01')
+    #     self.logger.info('运单{0}，进行司机确认发车操作，返回信息：{1}'.format(waybillID, confirmMsg.json()))
+    #     response = PaymentSinglePay().single_pay(waybillID, paymentMethod=2, amountType=1, amount='0.01',
+    #                                              password='123321')
+    #     self.logger.info('钱包付司机支付结果：{}'.format(response.json()))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.json()['code'], 0)
 
     def test_singlePay_OFFLINE(self):
         """ 运费支付方式：线下支付 """
