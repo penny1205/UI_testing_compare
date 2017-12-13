fd82537bb31a9e309389dcfc07519f398a871684
dwzhang <zhangdawei@keking.cn>
2017-12-13 13:45:50 +0800

    删除无用文件

--- util/http/httpclient.py
+++ util/http/httpclient.py
@@ -12,25 +12,25 @@ class HttpClient:
         requests.session().mount('https://',Ssl3Adapter())
 
     def get(self, url, header_dict=None, param_dict=None):
-        response = requests.get(url, headers=header_dict, params=param_dict, timeout=self.timeout)
+        response = requests.get(url, headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
         return response
 
     def post_form(self, url, body_dict=None, header_dict=None, param_dict=None):
-        response = requests.post(url, data=body_dict, headers=header_dict, params=param_dict, timeout=self.timeout)
+        response = requests.post(url, data=body_dict, headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
         return response
 
     def post_json(self, url, body_dict=None, header_dict=None, param_dict=None):
         header_dict['content-type'] = 'application/json'
-        response = requests.post(url, data=json.dumps(body_dict), headers=header_dict, params=param_dict, timeout=self.timeout)
+        response = requests.post(url, data=json.dumps(body_dict), headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
         return response
 
     def post_multipart(self, url, files=None, header_dict=None):
-        response = requests.post(url, files=files, headers=header_dict)
+        response = requests.post(url, files=files, headers=header_dict, verify=False)
         return response
 
     def post_multipart_file(self, url, file_path, header_dict=None):
         files = {'file': (open(file_path, 'rb'))}
-        response = requests.post(url, files=files, headers=header_dict)
+        response = requests.post(url, files=files, headers=header_dict, verify=False)
         return response
 
 if __name__ == '__main__':
