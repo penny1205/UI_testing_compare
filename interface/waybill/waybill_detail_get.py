936f51737a205e1986b6509e15ca77f999215c0e
panyuanyuan <panyuanyuan@keking.cn>
2018-01-04 09:51:00 +0800

    修改token统一的问题

--- interface/waybill/waybill_detail_get.py
+++ interface/waybill/waybill_detail_get.py
@@ -20,7 +20,9 @@ class WayBillDetailGet(object):
         self.__getWayBillDetailApiUrl = "https://{0}:{1}{2}/api/tms/wayBill/getWayBillDetail".format(
             config['tms_api_host'], config['tms_api_port'], config['tms_api_path'])
         self.__head_dict = {
-            'token': config['tms_api_token']}
+            'token': config['tms_api_token'],
+            'YD_OAUTH':config['tms_api_YD_OAUTH']
+        }
 
     def waybill_detail_get(self,wayBillId=''):
         '''获取运单详情'''
