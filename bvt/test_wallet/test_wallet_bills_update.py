673cc080e5bb0c9775af3e7bb82954b5306466f2
panyuanyuan <panyuanyuan@keking.cn>
2018-01-18 16:50:35 +0800

    修复bug

--- bvt/test_wallet/test_wallet_bills_update.py
+++ bvt/test_wallet/test_wallet_bills_update.py
@@ -19,21 +19,18 @@ class TestWalletBillsUpdate(unittest.TestCase):
 
     def test_wallet_bills_Update_success(self):
         '''修改交易状态'''
-        type = random.choice([1,2,3])
+        type = random.choice([1,2])
         bills_list = WalletBillsSelect().wallet_bills_select(type=type).json()['content']['dataList']
-        if bills_list == []:
+        if bills_list == None:
             self.logger.info('There is no transaction record for the wallet!')
         else:
-            L = []
-            for bills in bills_list:
-                L.append(bills)
-            bills_choice = random.choice(L)
+            bills_choice = random.choice(bills_list)
             self.logger.info('查询交易记录的流水号是{0},交易类型是{1}'.format(bills_choice['serialNumber'],bills_choice['type']))
             response = WalletBillsUpdate().wallet_bills_update(bills_choice['serialNumber'],bills_choice['type'])
             self.logger.info('交易记录查询返回状态码：{0}'.format(response))
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()['code'], 0)
             self.logger.info('交易记录查询返回结果是：{0}'.format(response.json()))
+            self.assertEqual(response.json()['code'], 0)
 
 if __name__ == '__main__':
     unittest.main()
