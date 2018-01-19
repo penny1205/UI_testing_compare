37a321ec790c9df9045fadb961b78c15de3fc01e
panyuanyuan <panyuanyuan@keking.cn>
2018-01-19 16:39:53 +0800

    添加case失败重试功能

--- util/http/httpclient.py
+++ util/http/httpclient.py
@@ -1,36 +1,80 @@
 # -*-coding:utf-8-*-
 import requests
 import json
+import time
 from util.http.sslAdapter import Ssl3Adapter
+from util.log.log import Log
 
 class HttpClient:
     '''发送http请求'''
 
     def __init__(self, timeout=60):
+        self.logger = Log()
         self.requests = requests
         self.timeout = timeout
         self.requests.session().mount('https://',Ssl3Adapter())
 
     def get(self, url, header_dict=None, param_dict=None):
-        response = requests.get(url, headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
+        response = None
+        while response == None:
+            try:
+                response = requests.get(url, headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
+            except requests.exceptions.ConnectionError as e:
+                self.logger.info("Connection refused by the server..{0}".format(e))
+                self.logger.info("Let me sleep for 3 seconds and try again")
+                time.sleep(3)
+                continue
         return response
 
     def post_form(self, url, body_dict=None, header_dict=None, param_dict=None):
-        response = requests.post(url, data=body_dict, headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
+        response = None
+        while response == None:
+            try:
+                response = requests.post(url, data=body_dict, headers=header_dict, params=param_dict,
+                                         timeout=self.timeout, verify=False)
+            except requests.exceptions.ConnectionError as e:
+                self.logger.info("Connection refused by the server..{0}".format(e))
+                self.logger.info("Let me sleep for 3 seconds and try again")
+                time.sleep(3)
+                continue
         return response
 
     def post_json(self, url, body_dict=None, header_dict=None, param_dict=None):
         header_dict['content-type'] = 'application/json'
-        response = requests.post(url, data=json.dumps(body_dict), headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
+        response = None
+        while response == None:
+            try:
+                response = requests.post(url, data=json.dumps(body_dict), headers=header_dict, params=param_dict, timeout=self.timeout, verify=False)
+            except requests.exceptions.ConnectionError as e:
+                self.logger.info("Connection refused by the server..{0}".format(e))
+                self.logger.info("Let me sleep for 3 seconds and try again")
+                time.sleep(3)
+                continue
         return response
 
     def post_multipart(self, url, files=None, header_dict=None):
-        response = requests.post(url, files=files, headers=header_dict, verify=False)
+        response = None
+        while response == None:
+            try:
+                response = requests.post(url, files=files, headers=header_dict, verify=False)
+            except requests.exceptions.ConnectionError as e:
+                self.logger.info("Connection refused by the server..{0}".format(e))
+                self.logger.info("Let me sleep for 3 seconds and try again")
+                time.sleep(3)
+                continue
         return response
 
     def post_multipart_file(self, url, file_path, header_dict=None):
         files = {'file': (open(file_path, 'rb'))}
-        response = requests.post(url, files=files, headers=header_dict, verify=False)
+        response = None
+        while response == None:
+            try:
+                response = requests.post(url, files=files, headers=header_dict, verify=False)
+            except requests.exceptions.ConnectionError as e:
+                self.logger.info("Connection refused by the server..{0}".format(e))
+                self.logger.info("Let me sleep for 3 seconds and try again")
+                time.sleep(3)
+                continue
         return response
 
 if __name__ == '__main__':
--- util/http/sslAdapter.py
+++ util/http/sslAdapter.py
@@ -4,7 +4,9 @@ import urllib3
 from requests.adapters import HTTPAdapter
 
 
+
 class Ssl3Adapter(HTTPAdapter):
     def __init__(self, num_pools=500,maxsize=2000):
-        self.poolmanager = urllib3.PoolManager(num_pools=num_pools, maxsize=maxsize, ssl_version=ssl.PROTOCOL_SSLv3)
+        retry = urllib3.Retry(connect=3, backoff_factor=0.5)
+        self.poolmanager = urllib3.PoolManager(num_pools=num_pools, maxsize=maxsize,retries=retry, ssl_version=ssl.PROTOCOL_SSLv3)
         self.disable_warnings = urllib3.disable_warnings()
